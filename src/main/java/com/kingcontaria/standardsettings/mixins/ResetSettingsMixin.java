d749916a90ba0f8da5604c63d5cf71dade3ddcac
KingContaria <98704937+KingContaria@users.noreply.github.com>
2022-05-15 13:43:33 +0200

    Add files via upload

--- src/main/java/com/kingcontaria/standardsettings/mixins/ResetSettingsMixin.java
+++ src/main/java/com/kingcontaria/standardsettings/mixins/ResetSettingsMixin.java
@@ -2,17 +2,17 @@ package com.kingcontaria.standardsettings.mixins;
 
 import com.kingcontaria.standardsettings.ResetSettings;
 import com.kingcontaria.standardsettings.StandardSettings;
-import net.minecraft.client.gui.screen.world.CreateWorldScreen;
-import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.world.level.LevelInfo;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(CreateWorldScreen.class)
+@Mixin(MinecraftClient.class)
 public class ResetSettingsMixin {
-    @Inject(method = "buttonClicked", at = @At(value = "INVOKE", target = "Ljava/util/Random;nextLong()J"))
-    void ResetSettings(ButtonWidget button, CallbackInfo ci) {
+    @Inject(method = "startGame", at = @At("HEAD"))
+    void ResetSettings(String string, String levelInfo, LevelInfo par3, CallbackInfo ci) {
         StandardSettings.LOGGER.info("Reset to StandardSettings...");
         ResetSettings.LoadStandardSettings();
     }
