e165ea329fec80062a2ee0ca6004ee918e66b4fe
KingContaria <98704937+KingContaria@users.noreply.github.com>
2022-06-05 11:14:54 +0200

    v1.1.2

---
+++ src/main/java/com/kingcontaria/standardsettings/mixins/MinecraftClientAccessor.java
@@ -0,0 +1,18 @@
+package com.kingcontaria.standardsettings.mixins;
+
+import net.minecraft.client.MinecraftClient;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin(MinecraftClient.class)
+
+public interface MinecraftClientAccessor {
+    @Accessor
+    String getOpenProfilerSection();
+    @Accessor
+    void setOpenProfilerSection(String value);
+
+    @Invoker
+    void callInitFont(boolean forcesUnicode);
+}
