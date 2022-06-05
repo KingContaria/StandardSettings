cbf683a4f456f3798c5b7acde8a3bb25b2032cc0
KingContaria <98704937+KingContaria@users.noreply.github.com>
2022-06-05 11:13:02 +0200

    v1.1.2

---
+++ src/main/java/com/kingcontaria/standardsettings/mixins/GameRendererMixin.java
@@ -0,0 +1,22 @@
+package com.kingcontaria.standardsettings.mixins;
+
+import com.kingcontaria.standardsettings.StandardSettings;
+import net.minecraft.client.render.GameRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(GameRenderer.class)
+
+public class GameRendererMixin {
+
+    @Inject(method = "method_9775", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MouseInput;updateMouse()V"))
+    private void changeSettingsOnJoin(float par1, CallbackInfo ci){
+        if(StandardSettings.changeOnGainedFocus){
+            StandardSettings.changeOnGainedFocus = false;
+            StandardSettings.changeSettingsOnJoin();
+        }
+    }
+
+}
