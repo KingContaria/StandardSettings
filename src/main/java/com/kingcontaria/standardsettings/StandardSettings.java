75f3c9b8a47de93e7fb909bd4d5d8680dafb3fb3
KingContaria <98704937+KingContaria@users.noreply.github.com>
2022-06-18 16:58:19 +0200

    forgot to copypaste one line

--- src/main/java/com/kingcontaria/standardsettings/StandardSettings.java
+++ src/main/java/com/kingcontaria/standardsettings/StandardSettings.java
@@ -74,7 +74,6 @@ public class StandardSettings {
                         case "bobView" -> options.bobView = Boolean.parseBoolean(strings[1]);
                         case "toggleCrouch" -> options.sneakToggled = Boolean.parseBoolean(strings[1]);
                         case "toggleSprint" -> options.sprintToggled = Boolean.parseBoolean(strings[1]);
-                        case "darkMojangStudiosBackground" -> options.monochromeLogo = Boolean.parseBoolean(strings[1]);
                         case "mouseSensitivity" -> options.mouseSensitivity = Double.parseDouble(strings[1]);
                         case "fov" -> options.fov = Double.parseDouble(strings[1]) * 40.0f + 70.0f;
                         case "screenEffectScale" -> options.distortionEffectScale = Float.parseFloat(strings[1]);
@@ -139,7 +138,7 @@ public class StandardSettings {
                         case "modelPart" -> {
                             for (PlayerModelPart playerModelPart : PlayerModelPart.values()) {
                                 if (strings[0].equals("modelPart_" + playerModelPart.getName())) {
-                                    options.togglePlayerModelPart(playerModelPart, Boolean.parseBoolean(strings[1])); break;
+                                    options.setPlayerModelPart(playerModelPart, Boolean.parseBoolean(strings[1])); break;
                                 }
                             }
                         }
@@ -161,7 +160,7 @@ public class StandardSettings {
         }
     }
 
-    public static void changeSettingsOnJoin(){
+    public static void changeSettingsOnJoin() {
         if (renderDistanceOnWorldJoin != 0) {
             Option.RENDER_DISTANCE.set(options, renderDistanceOnWorldJoin);
         }
@@ -281,6 +280,7 @@ public class StandardSettings {
                     case "fovOnWorldJoin" -> fov = line;
                 }
             }
+            standardoptionsTxt.close();
             Files.copy(optionsFile.toPath(), standardoptionsFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
             Files.write(standardoptionsFile.toPath(), ("perspective:" + options.getPerspective() + System.lineSeparator()).getBytes(), StandardOpenOption.APPEND);
             Files.write(standardoptionsFile.toPath(), ("piedirectory:" + ((MinecraftClientAccessor)client).getOpenProfilerSection().replace("",".") + System.lineSeparator()).getBytes(), StandardOpenOption.APPEND);
