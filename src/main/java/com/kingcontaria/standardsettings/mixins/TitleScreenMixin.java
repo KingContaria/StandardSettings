68b891a34cb312ca2577ec0345bfcb82840781f2
KingContaria <98704937+KingContaria@users.noreply.github.com>
2022-06-24 14:20:33 +0200

    v1.2.0-pre4

--- src/main/java/com/kingcontaria/standardsettings/mixins/TitleScreenMixin.java
+++ src/main/java/com/kingcontaria/standardsettings/mixins/TitleScreenMixin.java
@@ -16,18 +16,18 @@ import java.io.IOException;
 @Mixin(TitleScreen.class)
 
 public class TitleScreenMixin extends Screen {
-    private final Identifier SET_STANDARDSETTINGS_BUTTON_TEXTURE = new Identifier("textures/items/book_writable.png");
+    private static final Identifier SET_STANDARDSETTINGS_BUTTON_TEXTURE = new Identifier("textures/items/book_writable.png");
     private ButtonWidget SetStandardSettingsButton = null;
 
     @Inject(method = "initWidgetsNormal", at = @At("HEAD"))
     private void addCustomButton(int y, int spacingY, CallbackInfo ci){
         SetStandardSettingsButton = new ButtonWidget(420,this.width / 2 + 104, y, 20, 20, "");
-        this.buttons.add(SetStandardSettingsButton);
+        buttons.add(SetStandardSettingsButton);
     }
 
     @Inject(method = "render", at = @At("TAIL"))
     private void writableBookOverlay(int mouseX, int mouseY, float delta, CallbackInfo ci) {
-        this.client.getTextureManager().bindTexture(SET_STANDARDSETTINGS_BUTTON_TEXTURE);
+        client.getTextureManager().bindTexture(SET_STANDARDSETTINGS_BUTTON_TEXTURE);
         drawTexture(SetStandardSettingsButton.x+2, SetStandardSettingsButton.y+2, 0.0f, 0.0f, 16, 16, 16, 16);
     }
 
