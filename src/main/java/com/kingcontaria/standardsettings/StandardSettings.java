934a50fa7280b3aa8dd420b9320c70dacfcd1512
KingContaria <98704937+KingContaria@users.noreply.github.com>
2022-07-05 15:20:30 +0200

    v1.2.0-pre6

--- src/main/java/com/kingcontaria/standardsettings/StandardSettings.java
+++ src/main/java/com/kingcontaria/standardsettings/StandardSettings.java
@@ -12,16 +12,12 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
-import java.nio.file.StandardOpenOption;
-import java.util.Scanner;
 
 public class StandardSettings {
 
     public static final Logger LOGGER = LogManager.getLogger();
     private static final MinecraftClient client = MinecraftClient.getInstance();
-    private static final GameOptions options = client.options;
+    public static final GameOptions options = client.options;
     public static final File standardoptionsFile = new File("config/standardoptions.txt");
     public static final File optionsFile = new File("options.txt");
     public static boolean changeOnGainedFocus = false;
@@ -33,14 +29,23 @@ public class StandardSettings {
 
         fovOnWorldJoin = renderDistanceOnWorldJoin = 0;
 
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(standardoptionsFile))) {
+        try {
             if (!standardoptionsFile.exists()) {
                 LOGGER.error("standardoptions.txt is missing");
                 return;
             }
+
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(standardoptionsFile));
+
             boolean reload = false;
-            String string;
-            while ((string = bufferedReader.readLine()) != null) {
+            String string = bufferedReader.readLine();
+
+            if (new File(string).exists()) {
+                bufferedReader = new BufferedReader(new FileReader(string));
+                string = bufferedReader.readLine();
+            }
+
+            do {
                 String[] strings = string.split(":");
                 String[] string0_split = strings[0].split("_");
                 try {
@@ -101,11 +106,11 @@ public class StandardSettings {
                         }
                         case "forceUnicodeFont" -> client.textRenderer.method_960(options.forceUnicode = Boolean.parseBoolean(strings[1]));
                         case "clouds" -> options.renderClouds = Boolean.parseBoolean(strings[1]);
+                        case "hitboxes" -> EntityRenderDispatcher.field_5192 = Boolean.parseBoolean(strings[1]);
                         case "perspective" -> options.perspective = Integer.parseInt(strings[1]);
                         case "piedirectory" -> ((MinecraftClientAccessor)client).setOpenProfilerSection(strings[1]);
-                        case "hitboxes" -> EntityRenderDispatcher.field_5192 = Boolean.parseBoolean(strings[1]);
-                        case "renderDistanceOnWorldJoin" -> renderDistanceOnWorldJoin = Integer.parseInt(strings[1]);
                         case "fovOnWorldJoin" -> fovOnWorldJoin = Float.parseFloat(strings[1]);
+                        case "renderDistanceOnWorldJoin" -> renderDistanceOnWorldJoin = Integer.parseInt(strings[1]);
                         case "key" -> {
                             for (KeyBinding keyBinding : options.keysAll) {
                                 if (string0_split[1].equals(keyBinding.getTranslationKey())) {
@@ -121,15 +126,15 @@ public class StandardSettings {
                             }
                         }
                     }
-                    // Excluded is the 3D Anaglyph.
-                    // Additionally, options.txt settings which aren't accessible in vanilla Minecraft and some unnecessary settings (like Multiplayer and Streaming stuff) are not included.
+                    // Some options.txt settings which aren't accessible in vanilla Minecraft and some unnecessary settings (like Multiplayer and Streaming stuff) are not included.
                 } catch (Exception exception) {
-                    if (!string.equals("renderDistanceOnWorldJoin:") && !string.equals("fovOnWorldJoin:") && !string.equals("lastServer:")) {
+                    if (!string.equals("hitboxes:") && !string.equals("perspective:") && !string.equals("piedirectory:") && !string.equals("renderDistanceOnWorldJoin:") && !string.equals("fovOnWorldJoin:") && !string.equals("lastServer:")) {
                         LOGGER.warn("Skipping bad StandardSetting: " + string);
                     }
                 }
-            }
+            } while ((string = bufferedReader.readLine()) != null);
             KeyBinding.updateKeysByCode();
+            bufferedReader.close();
             if (reload) {
                 client.getSpriteAtlasTexture().load(client.getResourceManager());
             }
@@ -168,21 +173,13 @@ public class StandardSettings {
         options.chatHeightUnfocused = Check("(Chat) Unfocused Height", options.chatHeightUnfocused, 0, 1);
         options.chatScale = Check("Chat Text Size", options.chatScale, 0, 1);
         options.chatWidth = Check("Chat Width", options.chatWidth, 0, 1);
-        for (SoundCategory soundCategory : SoundCategory.values()) {
-            client.getSoundManager().updateSoundVolume(soundCategory, Check(soundCategory.getName(), options.getSoundVolume(soundCategory), 1, 1));
-            options.setSoundVolume(soundCategory, options.getSoundVolume(soundCategory));
-        }
-        boolean reload = false;
-        if (options.mipmapLevels < 0 || options.mipmapLevels > 4) {
-            client.getSpriteAtlasTexture().setMaxTextureSize(options.mipmapLevels = Check("Mipmap Levels", options.mipmapLevels, 0, 4));
-            reload = true;
-        }
-        if (options.field_7638 < 0 || options.field_7638 > 16) {
+        if (options.mipmapLevels != (options.mipmapLevels = Check("Mipmap Levels", options.mipmapLevels, 0, 4)) || options.field_7638 != (options.field_7638 = Check("Anisotropic Filtering", options.field_7638, 0, 16))) {
             client.getSpriteAtlasTexture().method_7004(options.field_7638);
-            reload = true;
-        }
-        if (reload) {
+            client.getSpriteAtlasTexture().setMaxTextureSize(options.mipmapLevels);
             client.getSpriteAtlasTexture().load(client.getResourceManager());
+        }
+        for (SoundCategory soundCategory : SoundCategory.values()) {
+            options.setSoundVolume(soundCategory, Check(soundCategory.getName(), options.getSoundVolume(soundCategory), 1, 1));
         }
 
         if (renderDistanceOnWorldJoin != 0) {
@@ -217,42 +214,5 @@ public class StandardSettings {
             return max;
         }
         return setting;
-    }
-
-    public static void save() {
-        LOGGER.info("Saving StandardSettings...");
-
-        long start = System.nanoTime();
-
-        if (!optionsFile.exists()) options.save();
-        if (!standardoptionsFile.getParentFile().exists()) standardoptionsFile.getParentFile().mkdir();
-
-        String rd = "renderDistanceOnWorldJoin:";
-        String fov = "fovOnWorldJoin:";
-
-        if (standardoptionsFile.exists()) {
-            try (Scanner standardoptionsTxt = new Scanner(standardoptionsFile)) {
-                while (standardoptionsTxt.hasNextLine()) {
-                    String line = standardoptionsTxt.nextLine();
-                    switch (line.split(":")[0]) {
-                        case "renderDistanceOnWorldJoin" -> rd = line;
-                        case "fovOnWorldJoin" -> fov = line;
-                    }
-                }
-            } catch (FileNotFoundException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-        try {
-            Files.copy(optionsFile.toPath(), standardoptionsFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
-            Files.write(standardoptionsFile.toPath(), ("perspective:" + options.perspective + System.lineSeparator()).getBytes(), StandardOpenOption.APPEND);
-            Files.write(standardoptionsFile.toPath(), ("piedirectory:" + ((MinecraftClientAccessor)client).getOpenProfilerSection() + System.lineSeparator()).getBytes(), StandardOpenOption.APPEND);
-            Files.write(standardoptionsFile.toPath(), ("hitboxes:" + EntityRenderDispatcher.field_5192 + System.lineSeparator()).getBytes(), StandardOpenOption.APPEND);
-            Files.write(standardoptionsFile.toPath(), (rd + System.lineSeparator() + fov).getBytes(), StandardOpenOption.APPEND);
-            LOGGER.info("Finished saving StandardSettings ({} ms)", (System.nanoTime() - start) / 1000000.0f);
-        } catch (IOException e) {
-            LOGGER.error("Failed to save StandardSettings", e);
-        }
     }
 }
--- src/main/java/com/kingcontaria/standardsettings/mixins/MinecraftClientMixin.java
+++ src/main/java/com/kingcontaria/standardsettings/mixins/MinecraftClientMixin.java
@@ -2,7 +2,6 @@ package com.kingcontaria.standardsettings.mixins;
 
 import com.kingcontaria.standardsettings.StandardSettings;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.options.GameOptions;
 import net.minecraft.world.level.LevelInfo;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -10,37 +9,55 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.StandardOpenOption;
 
 @Mixin(MinecraftClient.class)
 
 public abstract class MinecraftClientMixin {
 
-    @Shadow public GameOptions options;
     @Shadow public abstract boolean isWindowFocused();
 
     @Inject(method = "initializeGame", at = @At("TAIL"))
-    private void loadStandardSettings(CallbackInfo ci) {
-        File oldStandardoptionsFile = new File("standardoptions.txt");
-        if (!StandardSettings.standardoptionsFile.exists() && oldStandardoptionsFile.exists()){
-            StandardSettings.LOGGER.info("Moving standardoptions.txt to config folder...");
-            if (!StandardSettings.standardoptionsFile.getParentFile().exists()) StandardSettings.standardoptionsFile.getParentFile().mkdir();
-            oldStandardoptionsFile.renameTo(StandardSettings.standardoptionsFile);
+    private void initializeStandardSettings(CallbackInfo ci) {
+        if (StandardSettings.standardoptionsFile.exists()) {
+            StandardSettings.LOGGER.info("Loading StandardSettings...");
+            StandardSettings.load();
+            StandardSettings.LOGGER.info("Checking StandardSettings...");
+            StandardSettings.checkSettings();
+            StandardSettings.options.save();
+        } else {
+            StandardSettings.LOGGER.info("Create StandardSettings File...");
+
+            long start = System.nanoTime();
+
+            if (!StandardSettings.optionsFile.exists()) {
+                StandardSettings.options.save();
+            }
+            if (!StandardSettings.standardoptionsFile.getParentFile().exists()) {
+                StandardSettings.standardoptionsFile.getParentFile().mkdir();
+            }
+
+            try {
+                String l = System.lineSeparator();
+                Files.copy(StandardSettings.optionsFile.toPath(), StandardSettings.standardoptionsFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+                Files.write(StandardSettings.standardoptionsFile.toPath(), ("hitboxes" + l + "perspective:" + l + "piedirectory:" + l + "renderDistanceOnWorldJoin:" + l + "fovOnWorldJoin:").getBytes(), StandardOpenOption.APPEND);
+                StandardSettings.LOGGER.info("Finished creating StandardSettings File ({} ms)", (System.nanoTime() - start) / 1000000.0f);
+            } catch (IOException e) {
+                StandardSettings.LOGGER.error("Failed to create StandardSettings File", e);
+            }
         }
-        StandardSettings.LOGGER.info("Loading StandardSettings...");
-        StandardSettings.load();
-        StandardSettings.LOGGER.info("Checking StandardSettings...");
-        StandardSettings.checkSettings();
-        options.save();
     }
 
     @Inject(method = "startGame", at = @At("HEAD"))
-    private void ResetSettings(String string, String levelInfo, LevelInfo par3, CallbackInfo ci) {
+    private void resetSettings(String string, String levelInfo, LevelInfo par3, CallbackInfo ci) {
         StandardSettings.LOGGER.info("Reset to StandardSettings...");
         StandardSettings.load();
         StandardSettings.LOGGER.info("Checking Settings...");
         StandardSettings.checkSettings();
-        options.save();
+        StandardSettings.options.save();
     }
 
     @Inject(method = "startGame", at = @At("TAIL"))
