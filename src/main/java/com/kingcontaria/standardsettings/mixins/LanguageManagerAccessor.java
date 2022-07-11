772152e1c538e114315d506a017580978c20593a
KingContaria <98704937+KingContaria@users.noreply.github.com>
2022-07-11 14:54:26 +0200

    v1.2.0-pre9

---
+++ src/main/java/com/kingcontaria/standardsettings/mixins/LanguageManagerAccessor.java
@@ -0,0 +1,14 @@
+package com.kingcontaria.standardsettings.mixins;
+
+import net.minecraft.client.resource.language.LanguageDefinition;
+import net.minecraft.client.resource.language.LanguageManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import java.util.Map;
+
+@Mixin(LanguageManager.class)
+public interface LanguageManagerAccessor {
+    @Accessor
+    Map<String, LanguageDefinition> getField_6653();
+}
