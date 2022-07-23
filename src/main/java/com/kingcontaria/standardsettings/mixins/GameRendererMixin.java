3e17f1beabbcfa1769c7747df02295c2314c2d3a
KingContaria <98704937+KingContaria@users.noreply.github.com>
2022-07-23 05:03:53 +0200

    v1.2.0

--- src/main/java/com/kingcontaria/standardsettings/mixins/GameRendererMixin.java
+++ src/main/java/com/kingcontaria/standardsettings/mixins/GameRendererMixin.java
@@ -1,8 +1,10 @@
 package com.kingcontaria.standardsettings.mixins;
 
 import com.kingcontaria.standardsettings.StandardSettings;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.GameRenderer;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -10,6 +12,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(GameRenderer.class)
 
 public class GameRendererMixin {
+
+    @Shadow private MinecraftClient client;
 
     @Inject(method = "method_1331", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MouseInput;updateMouse()V"))
     private void changeSettingsOnJoin(float par1, CallbackInfo ci) {
